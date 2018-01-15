a3c6f34ac8dd20de7087f079ec66c8d607b10c2b
501-04-PC\501-04 <darlll124@gmail.com>
2018-01-15 12:48:49 +0900

    180115

---
+++ javaexam180110/src/Exam.java
@@ -0,0 +1,50 @@
+public class Exam
+{   public static void main(String[] args)
+//    System.out.println(""); >>java에서 글자를 출력하는 명령어
+//   {
+//       System.out.println("Programming! It's fun.");
+//       System.out.println("My name is Hong Gil Dong. ");
+//       System.out.println("l am 13 years old.");
+//       System.out.println("(@) (@)");
+//       System.out.println("(=^.^=)") ;
+//       System.out.println("(-m-m-)");
+//       System.out.println("I can program well.");
+//       System.out.println("Dreams come true");
+//       System.out.println("My height");
+//       System.out.println(170);
+//       System.out.println("My weight");
+//       System.out.println(68.600000);
+//       System.out.println(5+" Dan");
+//       System.out.println(5+" * "+2+" = "+10);
+//       System.out.println("          item          count          price");
+//       System.out.println("           pen"+"             "+20+"            "+100);
+//       System.out.println("          note"+"              "+5+"             "+95);
+//       System.out.println("        eraser"+"            "+110+"             "+97);
+//       System.out.println("My name is Hong");
+//       System.out.println("My hometown");
+//       System.out.println("Flowering mountain");
+//       System.out.println("Flowering mountain");
+//       System.out.println("TTTTTTTTTT");
+//       System.out.println("TTTTTTTTTT");
+//       System.out.println("    TT    ");
+//       System.out.println("    TT    ");
+//       System.out.println("    TT    ");
+//       System.out.println(49+" * "+0.2683+0+0+" = "+13.146700);
+//       System.out.println("kor"+ 90);
+//       System.out.println("mat"+ 80);
+//       System.out.println("eng"+ 100);
+//       System.out.println("sum"+ 270);
+//       System.out.println("avg"+ 90);
+//       System.out.println("          Seoul")
+//   }
+    {int num1;
+    num1=10;
+    int num2=20;
+    int num3=num1+num2;
+    System.out.println(num1+"+"+num2+"="+num3);
+
+     char ch1;
+     ch1='a';
+     System.out.println('a');}
+
+}
